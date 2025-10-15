1f93b2cf329e7acf8ce3f52207e3b278fe569371
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-10-16 00:31:45 +0300

    add Node struct

--- pkg/raft/node.go
+++ pkg/raft/node.go
@@ -1,10 +1,9 @@
 package raft
 
-
 import (
-
+	"math/rand"
+	"time"
 )
-
 
 // This are the roles of the raft server nodes
 // There are 3 different roles which are Follower Candidate and Leader
@@ -33,20 +32,80 @@ type VoteResponse struct {
 }
 
 
-// 
+// This is struct is use for these rpcs:
+// 1-> General heartbeat
+// 2-> Commands that comes from client with entries
+// 3-> Catch up message 
 type AppendEntries struct {
-	Term uint64
-	LeaderID int
-	LastLogTerm uint64
+	Term uint64 // Leaders Term
+	LeaderID int 
+	LastLogTerm uint64 
 	LastLogIndex uint64
 	Entries [][]byte
-	LeaderCommit uint64
+	LeaderCommit uint64 // Leaders commit index
 }
 
 type AppendEntriesResponse struct {
 	Term uint64
 	Success bool
-	LastIndex uint64
+	LastIndex uint64 // Candidates commit index
+}
+
+// Now this is a general message format
+type RpcMessage struct {
+	From int
+	To   int
+	Body any 
 }
 
 
+// Base raft servers Node structure
+type Node struct {
+	Id int
+
+	LastTerm uint64
+	LastIndex uint64
+
+	// Voted for is store the last voted candidate id
+	VotedFor int
+
+	// Peers id list
+	Peers []int
+	Role Role
+
+	Inbox chan RpcMessage
+	ApplyChan chan []byte
+	StopChan chan struct{}
+
+	ElectionTimer *time.Timer
+	HeartbeatTicker *time.Ticker
+
+}
+
+func NewNode(id int, peers []int) *Node{
+	rand.Seed(time.Now().UnixNano())
+	n := &Node{
+		Id: id,
+		Peers: peers,
+		Role: Follower,
+		VotedFor: -1,
+		Inbox: make(chan RpcMessage, 1024),
+		ApplyChan: make(chan []byte, 1024),
+		StopChan: make(chan struct{}),
+		ElectionTimer: time.NewTimer(jitterElection()),
+		HeartbeatTicker: nil,
+	}
+
+	return n
+}
+
+// jitterElection returns a randomized election timeout between 300ms and 600ms.
+// Raft uses this randomization so that followers do not start elections
+// at the same time (avoiding split votes). Each node gets a slightly different
+// timeout duration to improve cluster stability.
+// Also you change duration multiplyer for your own project
+func jitterElection() time.Duration {
+	durationMultiplyer := 300
+
+	return time.Duration(durationMultiplyer)*time.Millisecond + time.Duration(rand.Intn(durationMultiplyer))*time.Millisecond
+}

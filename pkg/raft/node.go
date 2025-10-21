6014f17caa083c368e00bcfd9dce6ebcb78f4f62
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-10-21 15:56:14 +0300

    Start to implement Write ahead log

--- pkg/raft/node.go
+++ pkg/raft/node.go
@@ -18,39 +18,37 @@ const (
 	Leader
 )
 
-
 // Vote request is used for rpc that send from Candidate to Followers
 // When a candidate wants to be a Leader they send Vote Requests to Followers to take enough vote
-type VoteRequest struct{
-	LastTerm uint64 // Last term is the last term of the candidates log file
-	LastIndex uint64 // Last index is the last index of the candidates log file
-	CandidateId int 
+type VoteRequest struct {
+	LastTerm    uint64 // Last term is the last term of the candidates log file
+	LastIndex   uint64 // Last index is the last index of the candidates log file
+	CandidateId int
 	// After candidate start voting they increase their term. Thats why we use both Last Term and Term
-	Term uint64 
+	Term uint64
 }
 
 type VoteResponse struct {
-	Term uint64
+	Term    uint64
 	IsVoted bool
 }
-
 
 // This is struct is use for these rpcs:
 // 1-> General heartbeat
 // 2-> Commands that comes from client with entries
-// 3-> Catch up message 
+// 3-> Catch up message
 type AppendEntries struct {
-	Term uint64 // Leaders Term
-	LeaderID int 
-	LastLogTerm uint64 
+	Term         uint64 // Leaders Term
+	LeaderID     int
+	LastLogTerm  uint64
 	LastLogIndex uint64
-	Entries []LogEntry
+	Entries      []LogEntry
 	LeaderCommit uint64 // Leaders commit index
 }
 
 type AppendEntriesResponse struct {
-	Term uint64
-	Success bool
+	Term      uint64
+	Success   bool
 	LastIndex uint64 // Candidates commit index
 }
 
@@ -58,63 +56,72 @@ type AppendEntriesResponse struct {
 type RpcMessage struct {
 	From int
 	To   int
-	Body any 
+	Body any
 }
-
 
 // Base raft servers Node structure
 type Node struct {
 	Id int
 
+	// Current term is used for current leadership term
+	// so this is important for elections
 	CurrentTerm uint64
-	LastTerm uint64
+
+	// These two members are last occurence in the
+	// Node's log file
+	LastTerm  uint64
 	LastIndex uint64
 
 	// Voted for is store the last voted candidate id
-	VotedFor int
+	VotedFor     int
 	Votes        int
-    ElectionTerm uint64
+	ElectionTerm uint64
 
 	// Peers id list
 	Peers []int
-	Role Role
+	Role  Role
 
 	Log         []LogEntry
+	// Last applied is the index number that 
+	// the last occurence inde in the log entry
+	// LastApplied must <= CommitIndex
 	LastApplied uint64
 
-	NextIndex   map[int]uint64 // only leader
-	MatchIndex  map[int]uint64
+	// These are used by only leader
+	NextIndex  map[int]uint64 
+	MatchIndex map[int]uint64
 
 	CommitIndex uint64
 
-	Inbox chan RpcMessage
+	Inbox     chan RpcMessage
 	ApplyChan chan []byte
-	StopChan chan struct{}
+	StopChan  chan struct{}
 
-	ElectionTimer *time.Timer
+	ElectionTimer   *time.Timer
 	HeartbeatTicker *time.Ticker
 
-	Router *LocalRouter
-
+	Router  *LocalRouter
+	Pending map[uint64]chan []byte
 }
 
-func NewNode(id int, peers []int, r*LocalRouter) *Node{
+func NewNode(id int, peers []int, r *LocalRouter) *Node {
 	rand.Seed(time.Now().UnixNano())
 	n := &Node{
-		Id: id,
-		Peers: peers,
-		Role: Follower,
-		VotedFor: -1,
-		Inbox: make(chan RpcMessage, 1024),
-		ApplyChan: make(chan []byte, 1024),
-		StopChan: make(chan struct{}),
-		ElectionTimer: time.NewTimer(jitterElection()),
+		Id:              id,
+		Peers:           peers,
+		Role:            Follower,
+		VotedFor:        -1,
+		Inbox:           make(chan RpcMessage, 1024),
+		ApplyChan:       make(chan []byte, 1024),
+		StopChan:        make(chan struct{}),
+		ElectionTimer:   time.NewTimer(jitterElection()),
 		HeartbeatTicker: nil,
-		Router: r,
-		Log: []LogEntry{{Term: 0, Index: 0, Type: EntryBarrier}},
-		LastApplied: 0,
-		LastIndex: 0,
-		LastTerm: 0,
+		Router:          r,
+		Log:             []LogEntry{{Term: 0, Index: 0, Type: EntryBarrier}},
+		LastApplied:     0,
+		LastIndex:       0,
+		LastTerm:        0,
+		Pending:         make(map[uint64]chan []byte),
 	}
 
 	return n
@@ -136,13 +143,13 @@ func (n *Node) Run(ctx context.Context) {
 		select {
 		case <-ctx.Done():
 			return
-		case <- n.StopChan:
+		case <-n.StopChan:
 			return
-		case <- n.ElectionTimer.C:
+		case <-n.ElectionTimer.C:
 			n.startElection()
-		case m := <- n.Inbox:
+		case m := <-n.Inbox:
 			n.handleRPC(m)
-		case <- n.heartbeatTick():
+		case <-n.heartbeatTick():
 			if n.Role == Leader {
 				n.broadcastHeartbeats()
 			}
@@ -150,7 +157,9 @@ func (n *Node) Run(ctx context.Context) {
 	}
 }
 
-func (n *Node) heartbeatTick() <- chan time.Time {
+// This is the timer that used by leader to send a heartbeat to the 
+// followers or candidates to tell them leader Node is still alive
+func (n *Node) heartbeatTick() <-chan time.Time {
 	if n.HeartbeatTicker == nil {
 
 		return nil
@@ -159,7 +168,11 @@ func (n *Node) heartbeatTick() <- chan time.Time {
 	return n.HeartbeatTicker.C
 }
 
+
+// If the timer of the Node is send message Nodes try to start
+// an election and request vote from all peers
 func (n *Node) startElection() {
+	// If this Node is still leader reset the election timer
 	if n.Role == Leader {
 		n.resetElection()
 		return
@@ -169,20 +182,22 @@ func (n *Node) startElection() {
 	n.Role = Candidate
 	n.VotedFor = n.Id
 
-	n.ElectionTerm = term   
-    n.Votes = 1 
+	n.ElectionTerm = term
+	n.Votes = 1
 
 	log.Printf("[n%d] start a new election for term=%d", n.Id, term)
 
 	currentTerm, currentIndex := n.LastLogTerm(), n.LastLogIndex()
 
+	// In here form term Node uses its CurrentTerm + 1
 	req := VoteRequest{
-		Term: term,
-		LastTerm: currentTerm,
-		LastIndex: currentIndex,
+		Term:        term,
+		LastTerm:    currentTerm,
+		LastIndex:   currentIndex,
 		CandidateId: n.Id,
 	}
 
+	// Request all peers in its peer list
 	for _, p := range n.Peers {
 		if p == n.Id {
 			continue
@@ -195,20 +210,31 @@ func (n *Node) startElection() {
 	n.resetElection()
 }
 
-func (n * Node) becomeLeader() {
+
+// If candidate have enough vote set itself as a leader
+func (n *Node) becomeLeader() {
 	n.Role = Leader
 
+	// Close its timer if this timer is not nil than 
+	// after a time leader start a new election for itself
 	if n.ElectionTimer != nil {
-        if !n.ElectionTimer.Stop() {
-            select { case <-n.ElectionTimer.C: default: }
-        }
-    }
+		if !n.ElectionTimer.Stop() {
+			select {
+			case <-n.ElectionTimer.C:
+			default:
+			}
+		}
+	}
 
+	// After every election leader set index to log replication
+	// Next Index is starting from last log index + 1 and 
+	// decreased by 1 if followers logs index is lesser than the 
+	// leaders log index
 	n.NextIndex = make(map[int]uint64, len(n.Peers))
 	n.MatchIndex = make(map[int]uint64, len(n.Peers))
 	li := n.LastLogIndex()
 	for _, p := range n.Peers {
-		n.NextIndex[p]  = li + 1
+		n.NextIndex[p] = li + 1
 		n.MatchIndex[p] = 0
 	}
 	n.MatchIndex[n.Id] = li
@@ -223,7 +249,6 @@ func (n * Node) becomeLeader() {
 	n.HeartbeatTicker = time.NewTicker(80 * time.Millisecond)
 	log.Printf("[n%d] became LEADER term=%d", n.Id, atomic.LoadUint64(&n.CurrentTerm))
 }
-
 
 // This function is a general function that set the nodes role as follower
 // It uses when a valid leader send a heartbeat or append an entrie or a candidate request a vote which term and index is enough
@@ -241,11 +266,16 @@ func (n *Node) becomeFollower(term uint64, votedFor int) {
 // Reset the election timer with a random value between 300ms and 600ms
 func (n *Node) resetElection() {
 	if !n.ElectionTimer.Stop() {
-		select { case <-n.ElectionTimer.C: default: }
+		select {
+		case <-n.ElectionTimer.C:
+		default:
+		}
 	}
 	n.ElectionTimer.Reset(jitterElection())
 }
 
+
+// 
 func (n *Node) handleRPC(m RpcMessage) {
 	switch msg := m.Body.(type) {
 	case AppendEntries:
@@ -261,18 +291,22 @@ func (n *Node) handleRPC(m RpcMessage) {
 	}
 }
 
+// After vote request if followers term is greater than 
+// candidates term candidate becomes follower again
+// If candidate is suitable to become leader and also
+// vote is masjority then Node become a leader
 func (n *Node) handleVoteResponse(from int, resp VoteResponse) {
-	
+
 	term := atomic.LoadUint64(&n.CurrentTerm)
 
 	if resp.Term > term {
-        n.becomeFollower(resp.Term, -1)
-        return
-    }
+		n.becomeFollower(resp.Term, -1)
+		return
+	}
 
 	if n.Role != Candidate || resp.Term != n.ElectionTerm {
-        return
-    }
+		return
+	}
 
 	if resp.IsVoted {
 		n.Votes++
@@ -292,11 +326,10 @@ func (n *Node) handleAppendEntries(from int, req AppendEntries) {
 	// If the term of the node is greater than the leader that means leader is an old leader so send an RPC with succes: false value
 	term := atomic.LoadUint64(&n.CurrentTerm)
 	if req.Term < term {
-		
+
 		n.sendRPC(RpcMessage{From: n.Id, To: from, Body: AppendEntriesResponse{Term: term, Success: false, LastIndex: n.LastLogIndex()}})
 		return
 	}
-	
 
 	// If the leader is valid, then resest the election timer and also set role as follower
 	if req.Term > term || n.Role != Follower {
@@ -306,7 +339,7 @@ func (n *Node) handleAppendEntries(from int, req AppendEntries) {
 
 	if req.LastLogIndex > n.LastLogIndex() {
 		n.sendRPC(RpcMessage{From: n.Id, To: from, Body: AppendEntriesResponse{
-			Term: n.CurrentTerm, Success: false, LastIndex: n.LastLogIndex(),
+			Term: atomic.LoadUint64(&n.CurrentTerm), Success: false, LastIndex: n.LastLogIndex(),
 		}})
 		return
 	}
@@ -317,7 +350,7 @@ func (n *Node) handleAppendEntries(from int, req AppendEntries) {
 			n.Log = n.Log[:req.LastLogIndex+1]
 			n.LastIndex, n.LastTerm = n.LastLogIndex(), n.LastLogTerm()
 			n.sendRPC(RpcMessage{From: n.Id, To: from, Body: AppendEntriesResponse{
-				Term: n.CurrentTerm, Success: false, LastIndex: n.LastLogIndex(),
+				Term: atomic.LoadUint64(&n.CurrentTerm), Success: false, LastIndex: n.LastLogIndex(),
 			}})
 			return
 		}
@@ -326,7 +359,7 @@ func (n *Node) handleAppendEntries(from int, req AppendEntries) {
 	if len(req.Entries) > 0 {
 		insertAt := req.LastLogIndex + 1
 		if insertAt <= n.LastLogIndex() {
-			n.Log = n.Log[:insertAt] 
+			n.Log = n.Log[:insertAt]
 		}
 		n.Log = append(n.Log, req.Entries...)
 		n.LastIndex, n.LastTerm = n.LastLogIndex(), n.LastLogTerm()
@@ -347,15 +380,12 @@ func (n *Node) handleAppendEntries(from int, req AppendEntries) {
 		n.CommitIndex = hi
 	}
 
-
 	n.sendRPC(RpcMessage{From: n.Id, To: from, Body: AppendEntriesResponse{
 		Term:      atomic.LoadUint64(&n.CurrentTerm),
 		Success:   true,
 		LastIndex: 0,
 	}})
 }
-
-
 
 func (n *Node) handleAppendEntriesResponse(from int, resp AppendEntriesResponse) {
 	if n.Role != Leader {
@@ -372,21 +402,10 @@ func (n *Node) handleAppendEntriesResponse(from int, resp AppendEntriesResponse)
 		if n.NextIndex[from] > 1 {
 			n.NextIndex[from]--
 		}
-		prevIdx := n.NextIndex[from] - 1
-		prevTerm, _ := n.TermAt(prevIdx)
-		req := AppendEntries{
-			Term:         n.CurrentTerm,
-			LeaderID:     n.Id,
-			LastLogIndex: prevIdx,
-			LastLogTerm:  prevTerm,
-			Entries:      nil,
-			LeaderCommit: n.CommitIndex,
-		}
-		n.sendRPC(RpcMessage{From: n.Id, To: from, Body: req})
+		n.ReplicateTo(from)
 		return
 	}
 
-	
 	if resp.LastIndex > n.MatchIndex[from] {
 		n.MatchIndex[from] = resp.LastIndex
 	}
@@ -405,7 +424,7 @@ func (n *Node) maybeAdvanceCommit() {
 		if n.Log[N].Term != n.CurrentTerm {
 			continue
 		}
-		count := 1 
+		count := 1
 		for _, p := range n.Peers {
 			if p == n.Id {
 				continue
@@ -427,14 +446,21 @@ func (n *Node) applyCommitted() {
 	for n.LastApplied < n.CommitIndex {
 		n.LastApplied++
 		e := n.Log[n.LastApplied]
+
 		if e.Type == EntryNormal && n.ApplyChan != nil {
 			n.ApplyChan <- e.Data
 		}
+
+		if w, ok := n.Pending[n.LastApplied]; ok {
+			select {
+			case w <- e.Data:
+			default:
+			}
+			close(w)
+			delete(n.Pending, n.LastApplied)
+		}
 	}
 }
-
-
-
 
 func (n *Node) handleRequestVote(from int, req VoteRequest) {
 	term := atomic.LoadUint64(&n.CurrentTerm)
@@ -478,7 +504,7 @@ func (n *Node) broadcastHeartbeats() {
 			LeaderID:     n.Id,
 			LastLogIndex: prevIdx,
 			LastLogTerm:  prevTerm,
-			Entries:      nil,              
+			Entries:      nil,
 			LeaderCommit: n.CommitIndex,
 		}
 
@@ -492,23 +518,29 @@ func (n *Node) sendRPC(m RpcMessage) {
 	}
 }
 
-
 func (n *Node) AppendLocal(e LogEntry) {
 	n.Log = append(n.Log, e)
 	n.LastIndex = e.Index
-	n.LastTerm  = e.Term
+	n.LastTerm = e.Term
 }
 
 func (n *Node) ReplicateTo(peer int) {
 	if n.Role != Leader {
 		return
 	}
+
 	next := n.NextIndex[peer]
 	prevIdx := next - 1
 	prevTerm, _ := n.TermAt(prevIdx)
 
-	entries := make([]LogEntry, n.LastLogIndex()-next+1)
-	copy(entries, n.Log[next:])
+	last := n.LastLogIndex()
+	var entries []LogEntry
+	if next <= last {
+		entries = make([]LogEntry, last-next+1)
+		copy(entries, n.Log[next:])
+	} else {
+		entries = nil
+	}
 
 	req := AppendEntries{
 		Term:         n.CurrentTerm,
---
+++ pkg/storage/wal.go
@@ -0,0 +1,299 @@
+package storage
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
+
+	"github.com/tidwall/tinylru"
+)
+
+var (
+	// ErrCorrupt Returns if the log is corrupt
+	ErrCorrupt = errors.New("log corrupt")
+
+	// ErrClosed is used when an operaiton cannot be completed
+	// if given log is closed
+	ErrClosed = errors.New("log closed")
+
+
+	// If an entry is not found return ErrNotFound
+	ErrNotFound = errors.New("not found")
+
+
+	// ErrOutOfOrder is returned from Write() when the index is not equal to
+	// LastIndex()+1. It's required that log monotonically grows by one and has
+	// no gaps. Thus, the series 10,11,12,13,14 is valid, but 10,11,13,14 is
+	// not because there's a gap between 11 and 13. Also, 10,12,11,13 is not
+	// valid because 12 and 11 are out of order.
+	ErrOutOfOrder = errors.New("out of order")
+
+
+	// ErrOutOfRange is returned from TruncateFront() and TruncateBack() when
+	// the index not in the range of the log's first and last index. Or, this
+	// may be returned when the caller is attempting to remove *all* entries;
+	// The log requires that at least one entry exists following a truncate.
+	ErrOutOfRange = errors.New("out of range")
+
+
+	// ErrEmptyLog is returned by Open() when the `AllowEmpty` option was not
+	// provided and log has been emptied due to the use of TruncateFront() or
+	// TruncateBack().
+	ErrEmptyLog = errors.New("empty log")
+)
+
+
+// Format of the log file
+type LogFormat byte
+
+
+const (
+	Binary LogFormat = 0
+
+	JSON LogFormat = 1
+)
+
+
+// Options for Log
+type Options struct {
+	// NoSync disables fsync after writes. This is less durable and puts the
+	// log at risk of data loss when there's a server crash.
+	NoSync bool
+	// SegmentSize of each segment. This is just a target value, actual size
+	// may differ. Default 20 MB
+	SegmentSize int
+	// LogFormat is the format of the log files. Default Binary
+	LogFormat LogFormat
+	// SegmentCacheSize is the maximum number of segments that will be held in
+	// memory for caching. Increasing this value may enhance performance for
+	// concurrent read operations. Default 1
+	SegmentCacheSize int
+	// NoCopy allows for the Read() operation to return the raw underlying data
+	// slice. This is an optimization to help minimize allocations. When this
+	// option is set, do not modify the returned data because it may affect
+	// other Read calls. Default false
+	NoCopy bool
+	// AllowEmpty allows for a log to have all entries removed through the use
+	// of TruncateFront() or TruncateBack(). Otherwise without this option,
+	// at least one entry must always remain following a truncate operation.
+	// Default false
+	//
+	// Warning: using this option changes the behavior of the log in the
+	// following ways:
+	// - An empty log will always have the FirstIndex() be equal to
+	//   LastIndex()+1.
+	// - For a newly created log that has no entries, FirstIndex() and
+	//   LastIndex() return 1 and 0, respectively.
+	//   Without AllowEmpty, both return 0.
+	AllowEmpty bool
+	// Perms represents the datafiles modes and permission bits
+	DirPerms  os.FileMode
+	FilePerms os.FileMode
+}
+
+
+var DefaultOptions = &Options{
+	NoSync:           false,    // Fsync after every write
+	SegmentSize:      20971520, // 20 MB log segment files
+	LogFormat:        Binary,   // Binary format is small and fast
+	SegmentCacheSize: 2,        // Number of cached in-memory segments
+	NoCopy:           false,    // Make a new copy of data for every Read call
+	AllowEmpty:       false,    // Do not allow empty log. 1+ entries required
+	DirPerms:         0750,     // Permissions for the created directories
+	FilePerms:        0640,     // Permissions for the created data files
+}
+
+type WAL struct {
+	mu         sync.RWMutex
+	path       string      // absolute path to log directory
+	opts       Options     // log options
+	closed     bool        // log is closed
+	corrupt    bool        // log may be corrupt
+	segments   []*segment  // all known log segments
+	firstIndex uint64      // index of the first entry in log
+	lastIndex  uint64      // index of the last entry in log
+	sfile      *os.File    // tail segment file handle
+	wbatch     Batch       // reusable write batch
+	scache     tinylru.LRU // segment entries cache
+}
+
+// segment represents a single segment file.
+type segment struct {
+	path  string // path of segment file
+	index uint64 // first index of segment
+	ebuf  []byte // cached entries buffer
+	epos  []bpos // cached entries positions in buffer
+}
+
+type bpos struct {
+	pos int // byte position
+	end int // one byte past pos
+}
+
+// Batch of entries. Used to write multiple entries at once using WriteBatch().
+type Batch struct {
+	entries []batchEntry
+	datas   []byte
+}
+
+type batchEntry struct {
+	index uint64
+	size  int
+}
+
+
+
+func Open(path string, opts *Options) (*WAL, error){
+	
+	if opts == nil {
+		opts = DefaultOptions
+	}
+	if opts.SegmentCacheSize <= 0 {
+		opts.SegmentCacheSize = DefaultOptions.SegmentCacheSize
+	}
+	if opts.SegmentSize <= 0 {
+		opts.SegmentSize = DefaultOptions.SegmentSize
+	}
+	if opts.DirPerms == 0 {
+		opts.DirPerms = DefaultOptions.DirPerms
+	}
+	if opts.FilePerms == 0 {
+		opts.FilePerms = DefaultOptions.FilePerms
+	}
+
+	var err error
+	path, err = filepath.Abs(path)
+	if(err != nil) {
+		return nil, err
+	}
+	w := &WAL{path: path, opts: *opts}
+	w.scache.Resize(opts.SegmentCacheSize)
+
+
+	err = os.MkdirAll(path, opts.DirPerms)
+	if(err != nil) {
+		return nil, err
+	}
+
+	files, err := os.ReadDir(w.path)
+	if err != nil {
+		return nil, err
+	}
+	startIdx := -1
+	endIdx := -1
+
+	for _, fl := range files {
+		name := fl.Name()
+		if fl.IsDir() || len(name) < 20 {
+			continue
+		}
+
+		index, err := strconv.ParseUint(name[:20], 10, 64)
+		if err != nil || index == 0 {
+			continue
+		}
+		isStart := len(name) == 26 && strings.HasSuffix(name, ".START")
+		isEnd := len(name) == 24 && strings.HasSuffix(name, ".END")
+		if len(name) == 20 || isStart || isEnd {
+			if isStart {
+				startIdx = len(w.segments)
+			} else if isEnd && endIdx == -1 {
+				endIdx = len(w.segments)
+			}
+			w.segments = append(w.segments, &segment{
+				index: index,
+				path:  filepath.Join(w.path, name),
+			})
+		}
+	}
+	if len(w.segments) == 0 {
+		w.segments = append(w.segments, &segment{
+			index: 1,
+			path: filepath.Join(w.path, segmentName(1)),
+		})
+
+		w.firstIndex = 1
+		w.lastIndex = 0
+		w.sfile, err = os.OpenFile(w.segments[0].path, 
+			os.O_CREATE|os.O_RDWR|os.O_TRUNC, w.opts.FilePerms)
+		return nil, err
+	}
+
+	if startIdx != -1 {
+		if endIdx != -1 {
+			return nil, ErrCorrupt
+		}
+
+		for i:= 0; i < startIdx; i++ {
+			err := os.Remove(w.segments[i].path);
+			if err != nil {
+				return nil, err
+			}
+		}
+
+		w.segments = append([]*segment{}, w.segments[startIdx:]...)
+
+		orgPath := w.segments[0].path
+		finalPath := orgPath[:len(orgPath)-len(".START")]
+		err := os.Rename(orgPath, finalPath)
+		if err != nil {
+			return nil, err
+		}
+		w.segments[0].path = finalPath
+	}
+
+	if endIdx != -1 {
+		// Delete all files following END
+		for i := len(w.segments) - 1; i > endIdx; i-- {
+			if err := os.Remove(w.segments[i].path); err != nil {
+				return nil, err
+			}
+		}
+		w.segments = append([]*segment{}, w.segments[:endIdx+1]...)
+		if len(w.segments) > 1 && w.segments[len(w.segments)-2].index ==
+			w.segments[len(w.segments)-1].index {
+			// remove the segment prior to the END segment because it shares
+			// the same starting index.
+			w.segments[len(w.segments)-2] = w.segments[len(w.segments)-1]
+			w.segments = w.segments[:len(w.segments)-1]
+		}
+		// Rename the END segment
+		orgPath := w.segments[len(w.segments)-1].path
+		finalPath := orgPath[:len(orgPath)-len(".END")]
+		err := os.Rename(orgPath, finalPath)
+		if err != nil {
+			return nil, err
+		}
+		w.segments[len(w.segments)-1].path = finalPath
+	}
+
+	w.firstIndex = w.segments[0].index
+
+	lseg := w.segments[len(w.segments) - 1]
+	w.sfile, err = os.OpenFile(lseg.path, os.O_WRONLY, w.opts.FilePerms)
+
+	if err != nil {
+		return nil, err
+	}
+	if _, err := w.sfile.Seek(0, 2); err != nil {
+		return nil, err
+	}
+	// Load the last segment entries
+	if err := w.loadSegmentEntries(lseg); err != nil {
+		return err
+	}
+	w.lastIndex = lseg.index + uint64(len(lseg.epos)) - 1
+	if w.lastIndex > 0 && w.firstIndex > w.lastIndex && !w.opts.AllowEmpty {
+		return nil, ErrEmptyLog
+	}
+
+	return w, nil
+}
+
+func segmentName(index uint64) string {
+	return fmt.Sprintf("%020d", index)
+}

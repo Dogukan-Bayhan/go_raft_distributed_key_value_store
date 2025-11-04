7140f4ea106c30102c8bb6202145737583f18dda
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-11-04 14:20:56 +0300

    command types

--- pkg/fsm/command.go
+++ pkg/fsm/command.go
@@ -1,1 +1,22 @@
 package fsm
+
+type OpCode uint8
+
+const (
+	OpPUT OpCode = iota
+	OpDEL
+	OpCAS       
+	OpBATCH     
+	OpTTLPut   
+	OpGCExpire  
+)
+
+type Command struct {
+	Version uint16   
+	Op      OpCode
+	Key     []byte
+	Value   []byte
+	CASOld  []byte     
+	TTLms   uint32    
+	Ops     []Command  
+}

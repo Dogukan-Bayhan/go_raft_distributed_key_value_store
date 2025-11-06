1526432cd416e55e9de1a468379e757951f5b95a
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-11-06 13:13:49 +0300

    change README

--- pkg/fsm/command.go
+++ pkg/fsm/command.go
@@ -1,22 +1,35 @@
 package fsm
 
+import (
+	"bytes"
+	"encoding/gob"
+)
+
 type OpCode uint8
 
 const (
 	OpPUT OpCode = iota
 	OpDEL
-	OpCAS       
-	OpBATCH     
-	OpTTLPut   
-	OpGCExpire  
+	OpCAS
+	OpBATCH
+	OpTTLPut
+	OpGCExpire
 )
 
 type Command struct {
-	Version uint16   
+	Version uint16
 	Op      OpCode
 	Key     []byte
 	Value   []byte
-	CASOld  []byte     
-	TTLms   uint32    
-	Ops     []Command  
+	CASOld  []byte
+	TTLms   uint32
+	Ops     []Command
+}
+
+func EncodeCommand(cmd Command) ([]byte, error) {
+	var buf bytes.Buffer
+	if err := gob.NewEncoder(&buf).Encode(cmd); err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }

86b2652de5561a1ed4fe98131299a9c0d4dcead6
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-11-13 21:55:43 +0300

    create fsm struct and metrics for tracing

--- pkg/fsm/fsm.go
+++ pkg/fsm/fsm.go
@@ -1,1 +1,75 @@
 package fsm
+
+
+func (f *FSM) Apply(msg ApplyMsg) interface{} {
+    cmd, err := DecodeCommand(msg.Data)
+    if err != nil { panic(err) }
+
+    switch cmd.Op {
+    case OpPUT:
+        f.applyPut(cmd)
+    case OpDEL:
+        f.applyDelete(cmd)
+    case OpTTLPut:
+        f.applyTTLPut(cmd)
+    case OpCAS:
+        ok := f.applyCAS(cmd)
+		return ok
+    case OpGCExpire:
+        f.applyExpire(cmd)
+    case OpBATCH:
+        f.applyBatch(cmd.Ops)
+    }
+
+    // update metrics
+    f.updateMetrics(cmd)
+	return nil
+}
+
+// --- METRICS ---
+
+func (f *FSM) updateMetrics(cmd Command) {
+    // Şimdilik boş
+}
+
+// --- APPLY OPERATIONS ---
+
+func (f *FSM) applyPut(cmd Command) {
+    // TODO: storage.Put
+}
+
+func (f *FSM) applyDelete(cmd Command) {
+    // TODO: storage.Delete
+}
+
+func (f *FSM) applyTTLPut(cmd Command) {
+    // TODO: TTL hesapla + storage.Put
+}
+
+func (f *FSM) applyExpire(cmd Command) {
+    // TODO: TTL expiration için delete
+}
+
+func (f *FSM) applyBatch(cmds []Command) {
+    // TODO: Apply every command
+    for _, c := range cmds {
+        switch c.Op {
+        case OpPUT:
+            f.applyPut(c)
+        case OpDEL:
+            f.applyDelete(c)
+        case OpTTLPut:
+            f.applyTTLPut(c)
+        case OpCAS:
+            _ = f.applyCAS(c)
+        case OpGCExpire:
+            f.applyExpire(c)
+        }
+    }
+}
+
+func (f *FSM) applyCAS(cmd Command) bool {
+    // TODO: compare-and-set
+    return false
+}
+
--- pkg/fsm/types.go
+++ pkg/fsm/types.go
@@ -30,3 +30,32 @@ type FSMOptions struct {
 	MaxBatchApply    int  
 	MetricsEnabled   bool 
 }
+
+type Metrics struct {
+    TotalKeys           uint64
+    TotalBytes          uint64
+    TTLKeys             uint64
+    
+    OpsTotal            uint64
+    OpsPut              uint64
+    OpsDelete           uint64
+    OpsGetLeader        uint64
+    OpsGetFollower      uint64
+    OpsExpired          uint64
+    
+    SnapshotCount       uint64
+    SnapshotLastSize    uint64
+        
+    ApplyCount          uint64
+    ApplyAvgLatencyNs   uint64
+}
+
+
+type Storage interface {
+	Get(key []byte) ([]byte, bool, error)
+}
+
+type FSM struct {
+	Storage *Storage
+	Metrics *Metrics
+}

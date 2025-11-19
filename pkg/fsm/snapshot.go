74251d39b924f072aa5efa1cbfdbd7131f0b8858
Dogukan-Bayhan <dogukanbayhan@hotmail.com>
2025-11-19 13:54:26 +0300

    snapshot functions

--- pkg/fsm/snapshot.go
+++ pkg/fsm/snapshot.go
@@ -1,2 +1,28 @@
 package fsm
 
+import (
+	"bytes"
+	"encoding/gob"
+	"hash/crc32"
+	"time"
+)
+
+func (f *FSM) CreateSnapshot(path string, meta SnapshotMeta) error {
+
+	kv, err := f.Storage.Snaphsot()
+	if err != nil {
+		return err
+	}
+
+	meta.Created = time.Now()
+
+	metaBuf := new(bytes.Buffer)
+
+	if err := gob.NewEncoder(metaBuf).Encode(meta); err != nil {
+		return err
+	}
+
+	metaBytes :=  metaBuf.Bytes()
+	metaSize := uint32(len(metaBytes))
+	metaChechksum := crc32.ChecksumIEEE(metaBytes)
+}
